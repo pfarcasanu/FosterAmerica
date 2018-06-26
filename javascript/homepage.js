94a059b066d13a76594ce0f93adac39a266234bf
Paul Farcasanu <pfarcasanu@gmail.com>
2018-06-26 17:32:04 -0500

    Changed bodyClicked()

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -1,14 +1,3 @@
 function bodyClicked(e) {
-    var targ;
-    if (!e) {
-        var e = window.event;
-    }
-    if (e.target) {
-        targ=e.target;
-    } else if (e.srcElement) {
-        targ=e.srcElement;
-    }
-    var tname;
-    tname = targ.tagName;
-    alert("You clicked on a " + tname + " element.");
+    window.location = "page2.html"
 }
