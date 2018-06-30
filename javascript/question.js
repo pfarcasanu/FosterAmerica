c6a35790375b86d93087b2ba55c06c4f94766b76
Paul Farcasanu <pfarcasanu@gmail.com>
2018-06-29 21:10:03 -0500

    Functional question.js (hardcoded)

--- javascript/question.js
+++ javascript/question.js
@@ -1,40 +1,36 @@
-/*let question = new QuizQuestion("What is bool?", ["A", "B", "C", "D"], "B");*/
-
-function UserDidAnswerA(){
-	//question.UserDidAnswer(question.answers()[0]);
-	Alert("user did press that button thoooo");
-}
-
-/*
-function UserDidAnswerB(){
-	question.UserDidAnswer(question.answers()[1]);
-}
-
-function UserDidAnswerC(){
-	question.UserDidAnswer(question.answers()[2]);
-}
-
-function UserDidAnswerD(){
-	question.UserDidAnswer(question.answers()[3]);
-}
-
 class QuizQuestion {
 
-	constructor(question, [answer_a, answer_b, answer_c, answer_d], correct_answer){
+	constructor(question, answers, correct_answer){
 		this.question = question;
 		this.answers = answers;
 		this.correct_answer = correct_answer;
 	}
 
-	get answers() {
-		return this._answers;
-	}
-
 	UserDidAnswer(choice){
-		if (choice == correct_answer) {
-			Alert("Right answer!");
+		if (choice == this.correct_answer) {
+			alert("Right answer!");
 		} else {
-			Alert("Wrong answer :(");
+			alert("Wrong answer :(");
 		}
 	}
-}*/
+}
+
+var answers = ["A", "B", "C", "D"];
+let question = new QuizQuestion("What is bool?", answers, "B");
+
+function UserDidAnswerA(){
+	question.UserDidAnswer(question.answers[0]);
+}
+
+function UserDidAnswerB(){
+	question.UserDidAnswer(question.answers[1]);
+}
+
+function UserDidAnswerC(){
+	question.UserDidAnswer(question.answers[2]);
+}
+
+function UserDidAnswerD(){
+	question.UserDidAnswer(question.answers[3]);
+}
+
