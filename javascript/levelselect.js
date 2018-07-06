cfa2327eff9cff661203926afc54e8b5392dab7f
williamwolf11 <wwolf@middlebury.edu>
2018-07-06 16:51:02 -0500

    Added links to the levelselect page

--- javascript/levelselect.js
+++ javascript/levelselect.js
@@ -1,36 +1,3 @@
-class QuizQuestion {
-
-	constructor(question, answers, correct_answer){
-		this.question = question;
-		this.answers = answers;
-		this.correct_answer = correct_answer;
-	}
-
-	UserDidAnswer(choice){
-		if (choice == this.correct_answer) {
-			alert("Right answer!");
-		} else {
-			alert("Wrong answer :(");
-		}
-	}
+function bodyClicked() {
+    window.location = "question2.html"
 }
-
-var answers = ["A", "B", "C", "D"];
-let question = new QuizQuestion("What is bool?", answers, "B");
-
-function UserDidAnswerA(){
-	question.UserDidAnswer(question.answers[0]);
-}
-
-function UserDidAnswerB(){
-	question.UserDidAnswer(question.answers[1]);
-}
-
-function UserDidAnswerC(){
-	question.UserDidAnswer(question.answers[2]);
-}
-
-function UserDidAnswerD(){
-	question.UserDidAnswer(question.answers[3]);
-}
-
