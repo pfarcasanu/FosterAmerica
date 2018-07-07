231dee98ab1e1e96ff657f9e5213635bd342cc6b
williamwolf11 <wwolf@middlebury.edu>
2018-07-07 15:03:32 -0500

    Completed design of level page

---
+++ javascript/level.js
@@ -0,0 +1,25 @@
+function fadeOut() {
+  document.getElementById('button1').fadeOut()
+}
+
+function closeDiv2() {
+  document.getElementById('button2').style.display = "none";
+}
+
+function closeDiv3() {
+  document.getElementById('button3').class = ".hidden";
+}
+
+function closeDiv4() {
+  document.getElementById('button4').style.display = "none";
+}
+
+function closeDiv5() {
+  document.getElementById('button5').style.display = "none";
+}
+
+function closeDiv6() {
+  document.getElementById('button6').style.display = "none";
+}
+
+
