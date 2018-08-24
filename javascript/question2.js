38d1db408a358da07e2b46d6c6e6e090081c4be0
Paul Farcasanu <pfarcasanu@gmail.com>
2018-08-24 14:08:05 -0500

    Minor fix?

--- javascript/question2.js
+++ javascript/question2.js
@@ -446,8 +446,8 @@ function setQuestionAnswered(cname, level, question, value){
   // pull the specific questions
   var question = getNumber(getCookie("question"));
   var level = getNumber(getCookie("level"));
-  const myQuestions = [questions_collect[level][question]];
-  //const myQuestions = [questions_collect[0][0]];
+  //const myQuestions = [questions_collect[level][question]];
+  const myQuestions = [questions_collect[0][0]];
 
   // the user hasn't gottena  question wrong yet
   var correctness = 2;
@@ -479,6 +479,7 @@ function setQuestionAnswered(cname, level, question, value){
       output.push(
         `<div class="slide">
            <div class="answers"> ${answers.join("")} </div>
+           </br>
          </div>`
       );
     });
