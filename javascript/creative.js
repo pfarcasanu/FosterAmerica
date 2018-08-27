75d7f10c4921935ec1a638f6f995e756f58610f7
Paul Farcasanu <pfarcasanu@gmail.com>
2018-08-27 14:03:10 -0500

    disclaimer stuff

--- javascript/creative.js
+++ javascript/creative.js
@@ -4,6 +4,7 @@ function buttonClick() {
   button_sound.play();
 
   button_sound.onended = function(){
+    document.cookie = "disclaimer=0";
     window.location.href = "disclaimer.html";
   }  
 }
@@ -16,107 +17,24 @@ function buttonClick2() {
   }  
 }
 
-function levelSelect() {
-  window.location = "levelselect_new.html";
-}
-
-/*function levelSelect2() {
-  window.setTimeout(levelSelect, 400)
-}*/
-
-
-//body scrolling that doesnt work rn 
-/*
-function stopBodyScrolling (bool) {
-  if (bool === true) {
-      document.body.addEventListener("touchmove", freezeVp, false);
-  } else {
-      document.body.removeEventListener("touchmove", freezeVp, false);
+window.onload = function(){
+  var disclaimer = getCookie("disclaimer");
+  if (disclaimer != ""){
+    document.getElementById("mybutton").onmousedown = buttonClick2();
   }
 }
 
-var freezeVp = function(e) {
-  e.preventDefault();
-};
-
-window.onload = stopBodyScrolling(true); */
-
-
-
-/* Built in boostrap code that i think we dont want!
-(function($) {
-  "use strict"; // Start of use strict
-
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 56)
-        }, 1000, "easeInOutExpo");
-        return false;
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+          c = c.substring(1);
       }
-    }
-  });
-
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
-
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 57
-  });
-
-  // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
-
-  /* Scroll reveal calls
-  window.sr = ScrollReveal();
-  sr.reveal('.sr-icons', {
-    duration: 600,
-    scale: 0.3,
-    distance: '0px'
-  }, 200);
-  sr.reveal('.sr-button', {
-    duration: 1000,
-    delay: 200
-  });
-  sr.reveal('.sr-contact', {
-    duration: 600,
-    scale: 0.3,
-    distance: '0px'
-  }, 300);
-
-  // Magnific popup calls
-  $('.popup-gallery').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
-  });
-
-})(jQuery); // End of use strict
-*/
+      if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+      }
+  }
+  return "";
+}
