0128f01d84232c68c24c70fc55b7fded262a5efa
williamwolf11 <wwolf@middlebury.edu>
2018-08-26 23:00:53 -0500

    Level system is UP

--- javascript/level.js
+++ javascript/level.js
@@ -10,8 +10,49 @@ if (level == 0) {
         y[z].style.backgroundColor = "red";
     }
     document.getElementsByClassName("navbar-brand1")[0].innerHTML = "Foster Care 101";
+    x.style.backgroundImage = "../images/fostercare101.png";
+}
+
+if (level == 1) {
+    for (z = 0; z < y.length; z++) {
+        y[z].style.backgroundColor = "orange";
+    }
+    document.getElementsByClassName("navbar-brand1")[0].innerHTML = "Facts and Figures";
     x.style.backgroundImage = "../images/header.jpg";
 }
+
+if (level == 2) {
+    for (z = 0; z < y.length; z++) {
+        y[z].style.backgroundColor = "yellow";
+    }
+    document.getElementsByClassName("navbar-brand1")[0].innerHTML = "Diary of an Orphan";
+    x.style.backgroundImage = "../images/header.jpg";
+}
+
+if (level == 3) {
+    for (z = 0; z < y.length; z++) {
+        y[z].style.backgroundColor = "green";
+    }
+    document.getElementsByClassName("navbar-brand1")[0].innerHTML = "Governments as Parents";
+    x.style.backgroundImage = "../images/header.jpg";
+}
+
+if (level == 4) {
+    for (z = 0; z < y.length; z++) {
+        y[z].style.backgroundColor = "blue";
+    }
+    document.getElementsByClassName("navbar-brand1")[0].innerHTML = "So You Turned 18";
+    x.style.backgroundImage = "../images/header.jpg";
+}
+
+if (level == 5) {
+    for (z = 0; z < y.length; z++) {
+        y[z].style.backgroundColor = "purple";
+    }
+    document.getElementsByClassName("navbar-brand1")[0].innerHTML = "A Problem We Can Solve";
+    x.style.backgroundImage = "../images/header.jpg";
+}
+
 for (i = 0; i<questions.length; i++){
     if (questions[i] != 0){
     document.getElementById(i).onclick = null;
