8a710388bb5f5fd3e01b72e237a115ee1637f34d
williamwolf11 <wwolf@middlebury.edu>
2018-08-26 22:31:22 -0500

    This is it bruh

--- javascript/level.js
+++ javascript/level.js
@@ -3,7 +3,10 @@ var str = getCookie("array");
 var level = parseInt(getCookie("level"));
 var questions = str.split("|")[level].split(",");
 if (level == 0) {
-    document.getElementsByClassName("box")[0,1,2,3].style.backgroundColor = "red";
+    document.getElementsByClassName("box")[0].style.backgroundColor = "red";
+    document.getElementsByClassName("box")[1].style.backgroundColor = "red";
+    document.getElementsByClassName("box")[2].style.backgroundColor = "red";
+    document.getElementsByClassName("box")[3].style.backgroundColor = "red";
     document.getElementsByClassName("grid-container")[0].style.backgroundImage = "url(fostercare101.png)";
     document.getElementsByClassName("navbar-brand1")[0].innerHTML = "Foster Care 101";
 }
