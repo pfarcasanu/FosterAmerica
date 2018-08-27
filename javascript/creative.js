5f407799376b7a88733e879caa6a53df412e3075
williamwolf11 <wwolf@middlebury.edu>
2018-08-27 14:28:00 -0500

    Shit idk

--- javascript/creative.js
+++ javascript/creative.js
@@ -13,8 +13,30 @@ function buttonClick2() {
   button_sound.play();
 
   button_sound.onended = function(){
-    window.location.href = "levelnew.html";
+    window.location.href = "levelselect_new.html";
   }  
 }
 
 
+window.onload = function(){
+  var disclaimer = getCookie("disclaimer");
+  if (disclaimer != ""){
+    document.getElementById("mybutton").onmousedown = buttonClick2();
+  }
+}
+
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+      }
+  }
+  return "";
+}
--- javascript/disclaimer.js
+++ javascript/disclaimer.js
@@ -5,16 +5,7 @@ function buttonClick() {
   button_sound.play();
 
   button_sound.onended = function(){
-    document.cookie = "disclaimer=0";
-    window.location.href = "disclaimer.html";
-  }  
-}
-
-function buttonClick2() {
-  button_sound.play();
-
-  button_sound.onended = function(){
-    window.location.href = "levelnew.html";
+    window.location.href = "levelselect_new.html";
   }  
 }
 
@@ -22,25 +13,5 @@ function testSound() {
     button_sound2.play();
 }
 
-window.onload = function(){
-  var disclaimer = getCookie("disclaimer");
-  if (disclaimer != ""){
-    document.getElementById("mybutton").onmousedown = buttonClick2();
-  }
-}
 
 
-function getCookie(cname) {
-  var name = cname + "=";
-  var ca = document.cookie.split(';');
-  for(var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-          c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-          return c.substring(name.length, c.length);
-      }
-  }
-  return "";
-}
