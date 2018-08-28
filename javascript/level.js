ce8c892b8d9c7943fc6f2050bc06a1044ad8ee5f
williamwolf11 <wwolf@middlebury.edu>
2018-08-28 01:48:18 -0500

    Shot in the dark

--- javascript/level.js
+++ javascript/level.js
@@ -5,100 +5,113 @@ var z;
 var str = getCookie("array");
 var level = parseInt(getCookie("level"));
 var questions = str.split("|")[level].split(",");
+var fanfair_sound = document.getElementById("fanFair"); 
 this.console.log(questions);
-
+    
 for (i = 0; i<questions.length; i++){
     if (questions[i] != 0){
     document.getElementById(i).onclick = null;
     document.getElementById(i).style.opacity = 0;
         }
     }
-
+    
+    
+    
 if (level == 0) {
-    ChangeBackgroundImageOfGrid("fostercare101");
+    ChangeBackgroundImageOfGrid("header");
+    if (questions.all(x => x != 0)){
+        fanfair_sound.play();
+        FadeBackground("fostercare101");
+    }
     document.getElementById("category").innerHTML = "Foster Care 101";
     for (z = 0; z < y.length; z++) {
         y[z].style.backgroundColor = "red";
-    }
+     }
 }
-    
+        
 if (level == 1) {
     for (z = 0; z < y.length; z++) {
         y[z].style.backgroundColor = "orange";
+        }
+        ChangeBackgroundImageOfGrid("header");
+        document.getElementById("category").innerHTML = "Facts and Figures";
     }
-    ChangeBackgroundImageOfGrid("header");
-    document.getElementById("category").innerHTML = "Facts and Figures";
-}
-
-if (level == 2) {
-    ChangeBackgroundImageOfGrid("header");
-    for (z = 0; z < y.length; z++) {
-        y[z].style.backgroundColor = "yellow";
-    }
-    document.getElementById("category").innerHTML = "Diary of an Orphan";
-}
-
-if (level == 3) {
-    ChangeBackgroundImageOfGrid("header");
-    for (z = 0; z < y.length; z++) {
-        y[z].style.backgroundColor = "green";
-    }
-    document.getElementById("category").innerHTML = "Governments as Parents";
-}
-
-if (level == 4) {
-    ChangeBackgroundImageOfGrid("header");
-    for (z = 0; z < y.length; z++) {
-        y[z].style.backgroundColor = "blue";
-    }
-    document.getElementById("category").innerHTML = "So You Turned 18";
-}
-
-if (level == 5) {
-    ChangeBackgroundImageOfGrid("header");
-    for (z = 0; z < y.length; z++) {
-        y[z].style.backgroundColor = "purple";
-    }
-    document.getElementById("category").innerHTML = "A Problem We Can Solve";
     
-}
-}
-      
-function setQuestionCookie(cname, new_question) {
-    document.cookie = cname + "=" +  new_question + ";";
-}
-
-function questionWasPressed(q_number) {
-    var cname = "question";
-    setQuestionCookie(cname, q_number);
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
+    if (level == 2) {
+        ChangeBackgroundImageOfGrid("header");
+        for (z = 0; z < y.length; z++) {
+            y[z].style.backgroundColor = "yellow";
         }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+        document.getElementById("category").innerHTML = "Diary of an Orphan";
     }
-    return "";
-}
-
-function questionClick(e){
-	var targ;
-    targ=e.srcElement;
-    targID = targ.id;
-    console.log("targID = " + targID);
-    console.log(typeof targID);
-    questionWasPressed(targID);
-    window.location = 'question2.html';
-}
-
-function ChangeBackgroundImageOfGrid(imagePrefix){
-    $('#grid').css('background-image','url(images/' + imagePrefix + '.png)');
-}
-
+    
+    if (level == 3) {
+        ChangeBackgroundImageOfGrid("header");
+        for (z = 0; z < y.length; z++) {
+            y[z].style.backgroundColor = "green";
+        }
+        document.getElementById("category").innerHTML = "Governments as Parents";
+    }
+    
+    if (level == 4) {
+        ChangeBackgroundImageOfGrid("header");
+        for (z = 0; z < y.length; z++) {
+            y[z].style.backgroundColor = "blue";
+        }
+        document.getElementById("category").innerHTML = "So You Turned 18";
+    }
+    
+    if (level == 5) {
+        ChangeBackgroundImageOfGrid("header");
+        for (z = 0; z < y.length; z++) {
+            y[z].style.backgroundColor = "purple";
+        }
+        document.getElementById("category").innerHTML = "A Problem We Can Solve";
+        
+    }
+    }
+          
+    function setQuestionCookie(cname, new_question) {
+        document.cookie = cname + "=" +  new_question + ";";
+    }
+    
+    function questionWasPressed(q_number) {
+        var cname = "question";
+        setQuestionCookie(cname, q_number);
+    }
+    
+    function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+    
+    function questionClick(e){
+        var targ;
+        targ=e.srcElement;
+        targID = targ.id;
+        console.log("targID = " + targID);
+        console.log(typeof targID);
+        questionWasPressed(targID);
+        window.location = 'question2.html';
+    }
+    
+    function ChangeBackgroundImageOfGrid(imagePrefix){
+        $('#grid').css('background-image','url(images/' + imagePrefix + '.png)');
+    }
+    
+    function FadeBackground(imagePrefix){
+        $('#grid').delay(1000).fadeTo('slow', 0.3, function(){
+        $(this).css('background-image', 'url(' + imagePrefix + '.png)');
+    })
+    }
+    
