d4734eb18f635a5d4f34b0d512d210f09d7a7e5e
Paul Farcasanu <pfarcasanu@gmail.com>
2018-12-16 17:03:22 -0600

    a lot of mute stuff

---
+++ javascript/mute.js
@@ -0,0 +1,63 @@
+// if a mute button exists, pull that element
+var mute_btn = document.getElementById("mute_btn");
+
+// Cookie Ops
+function getCookie(cname) {
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+// On load and On Click
+window.onload = function mutebtn_click(){
+  var muted = getCookie("mute");
+  if (muted == ""){
+    document.cookie = "muted=0";
+    muted = 0; 
+  }
+  muted = parseInt(muted);
+  MuteOperations(muted);
+  UpdateUI(muted);
+}
+
+// Muting and Unmuting
+function MuteOperations(muted){
+  var elems = document.querySelectorAll("video, audio");
+  if (muted){
+    [].forEach.call(elems, function(elem) { mute(elem); });
+  } else {
+    [].forEach.call(elems, function(elem) { unmute(elem); });
+  }
+}
+
+function UpdateUI(muted){
+  if (mute_btn != null){
+    if (muted){
+      mute_btn.innerHTML = "Unmute";
+    } else {
+      mute_btn.innerHTML = "Mute";
+    }
+  }
+}
+
+function mute(elem) {
+  elem.muted = true;
+  elem.pause();
+}
+
+function unmute(elem){
+  elem.muted = false;
+  elem.play();
+}
+
+
+
